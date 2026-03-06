176f0ace2018b0dc4635a75120a058b757cdcd9d
Víctor Laureano Vega <victorlaureanovega@gmail.com>
2026-03-06 11:44:43 -0600

    Review

--- sql_challenges/challenge-01/solution.sql
+++ sql_challenges/challenge-01/solution.sql
@@ -17,10 +17,10 @@ SELECT * FROM movies;
 SELECT * FROM movies WHERE id=6;
 
 -- 2.2
-SELECT * FROM movies WHERE year>=2000 and year<=2010;
+SELECT * FROM movies WHERE year >= 2000 and year <= 2010;
 
 -- 2.3
-SELECT * FROM movies WHERE NOT(year>=2000 and year<=2010);
+SELECT * FROM movies WHERE NOT(year >= 2000 and year <= 2010);
 
 -- 2.4
 SELECT * FROM movies LIMIT 5;
